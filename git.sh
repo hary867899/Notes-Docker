705810e1bf537564830e152c92e2f91432a01624
hary867899 <mhari4tech@gmail.com>
2024-05-20 15:22:13 +0530

    20/05/2024

--- git.sh
+++ git.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-git remote remove origin
+git remove remote origin
 commit='first push of everything'
-
-url="https://github.com/hary867899/Notes-Docker.git"
+url='https://github.com/hary8678/Notes.git'
 git init
 git add .
 git commit -m "$commit"
 git branch -M main
 git remote add origin $url
 git push -u origin main
-
